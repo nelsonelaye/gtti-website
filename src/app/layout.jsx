5af8fe04a7bb1b6c2bc4da0d150a9a32abce1b4d
nelsonelaye <nelsonelaye@gmail.com>
2026-05-06 20:30:08 +0100

    refactor: config metadata favicon for link previews

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,14 +6,27 @@ import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 export const metadata = {
   title: "Green Transformation Trailblazers Initiative",
   description: "Green Transformation Trailblazers Initiative",
+  icons: {
+    icon: "/images/circle.png",
+  },
+  openGraph: {
+    title: "Green Transformation Trailblazers Initiative",
+    description: "Green Transformation Trailblazers Initiative",
+    images: [
+      {
+        url: "/images/circle.png",
+        width: 200,
+        height: 200,
+        alt: "GTTI Logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/images/circle.png" />
-      </head>
+      <head></head>
       <body>
         <ReactQueryProvider>
           <Header />
